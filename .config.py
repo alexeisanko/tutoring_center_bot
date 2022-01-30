47b90f20facc4fb29236ceca0f621d1ae1a54c1d
alexeisanko <alexeisanko@gmail.com>
2022-01-30 22:17:46 +0300

    удален файл конфигурации

---
+++ .config.py
@@ -0,0 +1,13 @@
+
+
+# логин администратора
+ADMIN_LOGIN = 'login'
+ADMIN_PASSWORD = 'password'
+
+# настройка телеграмм бота
+TOKEN_API_TG = 'token'
+CHAT_ID_TG = 'chat_id'
+
+# настройка бота в вк
+TOKEN_API_VK = 'token'
+CHAT_ID_VK = 'chat_id'
--- config.py
+++
@@ -1,7 +0,0 @@
-
-TOKEN_API = 'Ваш токен'
-
-ADMIN_LOGIN = ''
-ADMIN_PASSWORD = ''
-
-CHAT_ID_TG = 'ID Телеграм чата'
